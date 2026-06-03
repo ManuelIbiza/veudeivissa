1dfe8aa382ed0b0236a3b103367c4e8be27d65ad
Manuel <m88perezt@gmail.com>
2026-06-03 11:47:36 +0200

    Corregir settings Cloudinary Render

--- projecte_veu/settings.py
+++ projecte_veu/settings.py
@@ -143,9 +143,13 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
+
+DEFAULT_FILE_STORAGE = "django.core.files.storage.FileSystemStorage"
 
 
 CLOUDINARY_STORAGE = {
@@ -156,6 +160,8 @@ CLOUDINARY_STORAGE = {
 
 
 if os.environ.get("RENDER"):
+    DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
+
     STORAGES = {
         "default": {
             "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
