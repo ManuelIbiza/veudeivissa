a7917a83034a152fd18cf7f821d9ab73db71a937
Manuel <m88perezt@gmail.com>
2026-06-03 09:42:10 +0200

    Redirigir raiz a español

--- projecte_veu/settings.py
+++ projecte_veu/settings.py
@@ -78,6 +78,7 @@ if os.environ.get("RENDER"):
         "default": dj_database_url.config(
             default=os.environ.get("DATABASE_URL"),
             conn_max_age=600,
+            ssl_require=True,
         )
     }
 else:
