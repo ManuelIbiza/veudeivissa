185f4d3b726f181d7e4bf976221b40abbb1bac82
Manuel <m88perezt@gmail.com>
2026-06-03 09:57:10 +0200

    Crear superusuario automaticamente en Render

--- build.sh
+++ build.sh
@@ -2,5 +2,26 @@
 set -o errexit
 
 pip install -r requirements.txt
+
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+python manage.py shell -c "
+import os
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+username = os.environ.get('DJANGO_SUPERUSER_USERNAME')
+email = os.environ.get('DJANGO_SUPERUSER_EMAIL')
+password = os.environ.get('DJANGO_SUPERUSER_PASSWORD')
+
+if username and email and password:
+    if not User.objects.filter(username=username).exists():
+        User.objects.create_superuser(username=username, email=email, password=password)
+        print('Superusuario creado correctamente.')
+    else:
+        print('El superusuario ya existe.')
+else:
+    print('Variables de superusuario no configuradas.')
+"
